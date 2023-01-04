03becf4673187f82dc092e23d4deb930f6fa0721
Mint <ar.chaonsri@gmail.com>
2023-01-04 14:20:25 +0700

    title category

--- pages/movie/category/_slug.vue
+++ pages/movie/category/_slug.vue
@@ -17,7 +17,7 @@ export default {
       head() {
         return {
             titleTemplate: (titleChunk) => {
-                return titleChunk ? this.typeObj.name_th + ` | ${titleChunk} ` : this.typeObj.name_th;
+                return titleChunk ? this.typeObj.name_th + ` | movie365` 
             },
             meta: [
                 {
